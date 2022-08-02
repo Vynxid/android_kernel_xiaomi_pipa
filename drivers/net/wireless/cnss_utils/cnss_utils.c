fb39cd2254590f2841c1a4465a2a85812b84e76f
Divyanshu-Modi <divyan.m05@gmail.com>
2022-08-02 15:17:43 +0530

    drivers: disable debugfs file creation when !CONFIG_DEBUG_FS
    Change-Id: I4e483e4e74a306ac9213887950eeaf4ce1165947
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/net/wireless/cnss_utils/cnss_utils.c
+++ drivers/net/wireless/cnss_utils/cnss_utils.c
@@ -311,6 +311,7 @@ enum cnss_utils_cc_src cnss_utils_get_cc_source(struct device *dev)
 }
 EXPORT_SYMBOL(cnss_utils_get_cc_source);
 
+#ifdef CONFIG_DEBUG_FS
 static ssize_t cnss_utils_mac_write(struct file *fp,
 				    const char __user *user_buf,
 				    size_t count, loff_t *off)
