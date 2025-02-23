987381f9226c5442b954b4882e63dac5a9c52779
Rudi Setiyawan <diphons@gmail.com>
2025-02-23 07:03:57 +0700

    touchscreen: fts_521: convert to use "struct proc_ops" Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -7136,9 +7136,9 @@ out:
 	return retval;
 }
 
-static const struct file_operations fts_selftest_ops = {
-	.read = fts_selftest_read,
-	.write = fts_selftest_write,
+static const struct proc_ops fts_selftest_ops = {
+	.proc_read = fts_selftest_read,
+	.proc_write = fts_selftest_write,
 };
 
 static ssize_t fts_datadump_read(struct file *file, char __user *buf,
@@ -7194,8 +7194,8 @@ out:
 	return cnt1 + cnt2 + cnt3;
 }
 
-static const struct file_operations fts_datadump_ops = {
-	.read = fts_datadump_read,
+static const struct proc_ops fts_datadump_ops = {
+	.proc_read = fts_datadump_read,
 };
 
 #define TP_INFO_MAX_LENGTH 50
@@ -7220,8 +7220,8 @@ static ssize_t fts_fw_version_read(struct file *file, char __user *buf,
 		return cnt;
 }
 
-static const struct file_operations fts_fw_version_ops = {
-	.read = fts_fw_version_read,
+static const struct proc_ops fts_fw_version_ops = {
+	.proc_read = fts_fw_version_read,
 };
 
 static ssize_t fts_lockdown_info_read(struct file *file, char __user *buf,
@@ -7255,8 +7255,8 @@ out:
 		return cnt;
 }
 
-static const struct file_operations fts_lockdown_info_ops = {
-	.read = fts_lockdown_info_read,
+static const struct proc_ops fts_lockdown_info_ops = {
+	.proc_read = fts_lockdown_info_read,
 };
 
 #ifdef CONFIG_PM
