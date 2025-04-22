3d09b215525ccac3d8a9996392cff9cf082f7b02
CuriousNom <aryanbiswas7585@gmail.com>
2025-04-22 18:16:34 +0530

    build: Only build for AOSP pipa
    * We have switched to diff branch for miui so remove them from here

--- README.md
+++ README.md
@@ -1,41 +1,9 @@
 # About this repo
-This repo (`android14-lineage21-mod` branch) is based on [Lineage OS 21 xiaomi sm8250 kernel source](https://github.com/LineageOS/android_kernel_xiaomi_sm8250).
-
-Originally this repo (`android12-stable-mod` or `android14-stable-mod` branch) is fork from [UtsavBalar1231's repo](https://github.com/UtsavBalar1231/kernel_xiaomi_sm8250), but the `android14-stable` branch has freeze problem when the device sleep for a while or wake up. So now I switched to lineage21 codebase. The MIUI features code and some parts of the drivers is still copied from UtsavBalar1231's branch.
-
-So still Thanks to [@UtsavBalar1231](https://github.com/UtsavBalar1231/)!
+This repo is based on [Lineage OS 21 xiaomi sm8250 kernel source](https://github.com/LineageOS/android_kernel_xiaomi_sm8250).
 
 The main purpose of maintaining and building this kernel is to fix [this battery stuck at 1% problem](https://github.com/liyafe1997/Xiaomi-fix-battery-one-percent), and provide KernelSU integrated pre-built image(flashable anykernel3 zip). Also provides a more intuitive and easy-to-use build script and build guide that allow you to try to build by yourself.
 
-(The devices affected by the "1% battery bug" are: alioth, apollo, lmi, thyme, umi, pipa. Because they all use the PM8150, aka Qualcomm fuel gauge GEN4. For the other devices are not affected by that bug, you can use this kernel for KernelSU purpose, as a replacement of the orginal stock kernel. Also, as the people tested, this kernel NoKernelSU version is good for applying [APatch](https://github.com/bmax121/APatch)).
-
-The pre-built kernel image/zip is built on the `android14-lineage21-mod` branch. It should works on both stock MIUI and third-party AOSP based Android 11-14 ROMs. You are welcomed to give feedback (Issues/Pull Requests)!
-
-Note: The zip does not include the `dtbo.img` and it will not replace your `dtbo` partition. It is recommanded to keep the stock `dtbo` or the `dtbo` from your third-party rom (If the builder comfirmed it works well). Since there are some problems with the `dtbo.img` which built from this source, one of them is the screen will suddently goes to the highest brightness when shut try to shut off the screen in the lock screen. If you had flashed any other third-party kernels, and you got some weird problem, you should keep an eye to check your `dtbo` has been replaced or not. 
-
-Supported devices:
-| Code Name | Device Name                          |
-|-----------|--------------------------------------|
-| psyche    | Xiaomi Mi 12X                        |
-| thyme     | Xiaomi Mi 10S                        |
-| umi       | Xiaomi Mi 10                         |
-| munch     | Poco F4 / Redmi K40S                 |
-| lmi       | Redmi K30 Pro                        |
-| cmi       | Xiaomi Mi 10 Pro                     |
-| cas       | Xiaomi Mi 10 Ultra                   |
-| apollo    | Xiaomi Mi 10T / Redmi K30S Ultra     |
-| alioth    | Xiaomi Mi 11X / POCO F3 / Redmi K40  |
-| elish     | Xiaomi Pad 5 Pro                     |
-| enuma     | Xiaomi Pad 5 Pro 5G                  |
-| dagu      | Xiaomi Pad 5 Pro 12.4                |
-| pipa      | Xiaomi Pad 6                         |
-
-Other Features/Improvement of this Kernel:
-1. Support USB Serial (CH340/FTDI/PL2303/OTI6858/TI/SPCP8X5/QT2/UPD78F0730/CP210X).
-2. Support EROFS.
-3. F2FS realtime discard enabled for better TRIM the flash.
-4. Support CANBus and USB CAN adapter (like CANable).
-5. Support LZ4, LZ4HC, ZSTD compression algorithms for ZRAM.
+The devices affected by the "1% battery bug" are: alioth, apollo, lmi, thyme, umi, pipa. Because they all use the PM8150, aka Qualcomm fuel gauge GEN4. For the other devices are not affected by that bug, you can use this kernel for KernelSU purpose, as a replacement of the orginal stock kernel.
 
 # How to build
 1. Prepair the basic build environment. 
@@ -47,14 +15,6 @@ Other Features/Improvement of this Kernel:
     sudo apt update
     sudo apt install bc binutils-dev bison build-essential ca-certificates ccache clang cmake cpio curl file flex git libelf-dev libssl-dev lld make ninja-build python3-dev texinfo u-boot-tools xz-utils zlib1g-dev
     ```
-    And also, you have to have `python` (only `python3` is not enough). you can install the apt package `python-is-python3`.
-
-    In RHEL/RPM based OS, you can
-    ```
-    sudo yum groupinstall 'Development Tools'
-    sudo yum install wget bc openssl-devel ccache
-    ```
-
     Notice: `ccache` is enabled in `build.sh` for speed up the compiling. `CCACHE_DIR` has been set as `$HOME/.cache/ccache_mikernel` in `build.sh`. If you don't like you can remove or modify it.
 
 2. Download [ZYC-clang] compiler toolchain or any latest toolchain
@@ -64,7 +24,7 @@ Other Features/Improvement of this Kernel:
     ```
     mkdir tc
     cd tc
-    wget https://github.com/ZyCromerZ/Clang/releases/download/21.0.0git-20250308-release/Clang-21.0.0git-20250308.tar.gz
+    wget https://github.com/ZyCromerZ/Clang/releases/download/21.0.0git-20250422-release/Clang-21.0.0git-20250422.tar.gz
     tar -xvzf Clang-21.0.0git-*
     cd ..
     ```
@@ -73,14 +33,5 @@ Other Features/Improvement of this Kernel:
 
     Build Kernel: 
     ```
-    bash build.sh TARGET_DEVICE
+    bash build.sh
     ```
-
-    For example, build for pipa (Xiaomi Pad 6):
-    ```
-    bash build.sh pipa
-    ````
-
-    And also, here is a `buildall.sh` can build for all supported models at once.
-
-
--- build.sh
+++ build.sh
@@ -7,25 +7,6 @@ TOOLCHAIN_PATH=$(pwd)/tc/bin
 GIT_COMMIT_ID=$(git rev-parse --short=8 HEAD)
 TARGET_DEVICE=$1
 
-if [ -z "$1" ]; then
-    echo "Error: No argument provided, please specify a target device."
-    echo "Examples:"
-    echo "Build for pipa(Xiaomi Pad 6):"
-    echo "    bash build.sh pipa"
-    exit 1
-fi
-
-# Ask user which build to run
-echo "Choose build type:"
-echo "1) AOSP"
-echo "2) MIUI/HOS"
-read -p "Enter 1 or 2: " build_choice
-
-if [[ "$build_choice" != "1" && "$build_choice" != "2" ]]; then
-    echo "Invalid choice. Exiting."
-    exit 1
-fi
-
 if [ ! -d $TOOLCHAIN_PATH ]; then
     echo "TOOLCHAIN_PATH [$TOOLCHAIN_PATH] does not exist."
     echo "Please ensure the toolchain is there, or change TOOLCHAIN_PATH in the script to your toolchain path."
@@ -50,15 +31,14 @@ if ! command -v clang >/dev/null 2>&1; then
     exit 1
 fi
 
-# Enable ccache for speed up compiling 
-export CCACHE_DIR="$HOME/.cache/ccache_mikernel" 
+# Enable ccache for speed up compiling
+export CCACHE_DIR="$HOME/.cache/ccache_mikernel"
 export CC="ccache gcc"
 export CXX="ccache g++"
 export PATH="/usr/lib/ccache:$PATH"
 echo "CCACHE_DIR: [$CCACHE_DIR]"
 
-MAKE_ARGS_AOSP="ARCH=arm64 O=out CC=clang LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip"
-MAKE_ARGS="ARCH=arm64 O=out CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi-"
+MAKE_ARGS="ARCH=arm64 O=out CC=clang LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip"
 
 if [ "$1" == "j1" ]; then
     make $MAKE_ARGS -j1
@@ -70,13 +50,6 @@ if [ "$1" == "continue" ]; then
     exit
 fi
 
-if [ ! -f "arch/arm64/configs/${TARGET_DEVICE}_defconfig" ]; then
-    echo "No target device [${TARGET_DEVICE}] found."
-    echo "Available defconfigs, please choose one target from below down:"
-    ls arch/arm64/configs/*_defconfig
-    exit 1
-fi
-
 # Check clang is existing.
 echo "[clang --version]:"
 clang --version
@@ -85,7 +58,6 @@ clang --version
 export KBUILD_BUILD_USER="aryan"
 export KBUILD_BUILD_HOST="stormvault"
 
-echo "TARGET_DEVICE: $TARGET_DEVICE"
 echo "Cleaning..."
 rm -rf out/
 rm -rf error.log
@@ -98,12 +70,11 @@ else
     git clone https://github.com/liyafe1997/AnyKernel3 -b kona --single-branch --depth=1 anykernel
 fi
 
-if [ "$build_choice" == "1" ]; then
     # ------------- Building for AOSP -------------
     echo "Building for AOSP......"
-    make $MAKE_ARGS_AOSP ${TARGET_DEVICE}_aosp_defconfig
+    make $MAKE_ARGS pipa_aosp_defconfig
 
-    make $MAKE_ARGS_AOSP -j$(nproc --all) 2> >(tee -a error.log >&2)
+    make $MAKE_ARGS -j$(nproc --all) 2> >(tee -a error.log >&2)
 
     if [ -f "out/arch/arm64/boot/Image" ]; then
         echo "The file [out/arch/arm64/boot/Image] exists. AOSP Build successfully."
@@ -121,102 +92,11 @@ if [ "$build_choice" == "1" ]; then
     cp out/arch/arm64/boot/dtb anykernel/kernels/
 
     cd anykernel
-    ZIP_FILENAME=Kernel_BloodReaper_AOSP_${TARGET_DEVICE}_$(date +'%Y%m%d_%H%M%S')_anykernel3_${GIT_COMMIT_ID}.zip
+    ZIP_FILENAME=Kernel_BloodReaper_AOSP_pipa_$(date +'%Y%m%d_%H%M%S')_anykernel3_${GIT_COMMIT_ID}.zip
     zip -r9 $ZIP_FILENAME ./* -x .git .gitignore out/ ./*.zip
     mv $ZIP_FILENAME ../
     cd ..
 
     echo "Build for AOSP finished."
-
-elif [ "$build_choice" == "2" ]; then
-    # ------------- Building for MIUI/HOS -------------
-    echo "Clearing [out/] and building for MIUI/HOS....."
-    rm -rf out/
-
-dts_source=arch/arm64/boot/dts/vendor/qcom
-
-# Backup dts
-cp -a ${dts_source} .dts.bak
-
-# Correct panel dimensions on MIUI builds
-sed -i 's/<154>/<1537>/g' ${dts_source}/dsi-panel-j1s*
-sed -i 's/<154>/<1537>/g' ${dts_source}/dsi-panel-j2*
-sed -i 's/<155>/<1544>/g' ${dts_source}/dsi-panel-j3s-37-02-0a-dsc-video.dtsi
-sed -i 's/<155>/<1545>/g' ${dts_source}/dsi-panel-j11-38-08-0a-fhd-cmd.dtsi
-sed -i 's/<155>/<1546>/g' ${dts_source}/dsi-panel-k11a-38-08-0a-dsc-cmd.dtsi
-sed -i 's/<155>/<1546>/g' ${dts_source}/dsi-panel-l11r-38-08-0a-dsc-cmd.dtsi
-sed -i 's/<70>/<695>/g' ${dts_source}/dsi-panel-j11-38-08-0a-fhd-cmd.dtsi
-sed -i 's/<70>/<695>/g' ${dts_source}/dsi-panel-j3s-37-02-0a-dsc-video.dtsi
-sed -i 's/<70>/<695>/g' ${dts_source}/dsi-panel-k11a-38-08-0a-dsc-cmd.dtsi
-sed -i 's/<70>/<695>/g' ${dts_source}/dsi-panel-l11r-38-08-0a-dsc-cmd.dtsi
-sed -i 's/<71>/<710>/g' ${dts_source}/dsi-panel-j1s*
-sed -i 's/<71>/<710>/g' ${dts_source}/dsi-panel-j2*
-
-# Enable back mi smartfps while disabling qsync min refresh-rate
-sed -i 's/\/\/ mi,mdss-dsi-pan-enable-smart-fps/mi,mdss-dsi-pan-enable-smart-fps/g' ${dts_source}/dsi-panel*
-sed -i 's/\/\/ mi,mdss-dsi-smart-fps-max_framerate/mi,mdss-dsi-smart-fps-max_framerate/g' ${dts_source}/dsi-panel*
-sed -i 's/\/\/ qcom,mdss-dsi-pan-enable-smart-fps/qcom,mdss-dsi-pan-enable-smart-fps/g' ${dts_source}/dsi-panel*
-sed -i 's/qcom,mdss-dsi-qsync-min-refresh-rate/\/\/qcom,mdss-dsi-qsync-min-refresh-rate/g' ${dts_source}/dsi-panel*
-
-# Enable back refresh rates supported on MIUI
-sed -i 's/120 90 60/120 90 60 50 30/g' ${dts_source}/dsi-panel-g7a-36-02-0c-dsc-video.dtsi
-sed -i 's/120 90 60/120 90 60 50 30/g' ${dts_source}/dsi-panel-g7a-37-02-0a-dsc-video.dtsi
-sed -i 's/120 90 60/120 90 60 50 30/g' ${dts_source}/dsi-panel-g7a-37-02-0b-dsc-video.dtsi
-sed -i 's/144 120 90 60/144 120 90 60 50 48 30/g' ${dts_source}/dsi-panel-j3s-37-02-0a-dsc-video.dtsi
-
-
-# Enable back brightness control from dtsi
-sed -i 's/\/\/39 00 00 00 00 00 03 51 03 FF/39 00 00 00 00 00 03 51 03 FF/g' ${dts_source}/dsi-panel-j9-38-0a-0a-fhd-video.dtsi
-sed -i 's/\/\/39 00 00 00 00 00 03 51 0D FF/39 00 00 00 00 00 03 51 0D FF/g' ${dts_source}/dsi-panel-j2-p2-1-38-0c-0a-dsc-cmd.dtsi
-sed -i 's/\/\/39 00 00 00 00 00 05 51 0F 8F 00 00/39 00 00 00 00 00 05 51 0F 8F 00 00/g' ${dts_source}/dsi-panel-j1s-42-02-0a-dsc-cmd.dtsi
-sed -i 's/\/\/39 00 00 00 00 00 05 51 0F 8F 00 00/39 00 00 00 00 00 05 51 0F 8F 00 00/g' ${dts_source}/dsi-panel-j1s-42-02-0a-mp-dsc-cmd.dtsi
-sed -i 's/\/\/39 00 00 00 00 00 05 51 0F 8F 00 00/39 00 00 00 00 00 05 51 0F 8F 00 00/g' ${dts_source}/dsi-panel-j2-mp-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 00 00 00 00 00 05 51 0F 8F 00 00/39 00 00 00 00 00 05 51 0F 8F 00 00/g' ${dts_source}/dsi-panel-j2-p2-1-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 00 00 00 00 00 05 51 0F 8F 00 00/39 00 00 00 00 00 05 51 0F 8F 00 00/g' ${dts_source}/dsi-panel-j2s-mp-42-02-0a-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 00 00/39 01 00 00 00 00 03 51 00 00/g' ${dts_source}/dsi-panel-j2-38-0c-0a-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 00 00/39 01 00 00 00 00 03 51 00 00/g' ${dts_source}/dsi-panel-j2-38-0c-0a-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 03 FF/39 01 00 00 00 00 03 51 03 FF/g' ${dts_source}/dsi-panel-j11-38-08-0a-fhd-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 03 FF/39 01 00 00 00 00 03 51 03 FF/g' ${dts_source}/dsi-panel-j9-38-0a-0a-fhd-video.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 07 FF/39 01 00 00 00 00 03 51 07 FF/g' ${dts_source}/dsi-panel-j1u-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 07 FF/39 01 00 00 00 00 03 51 07 FF/g' ${dts_source}/dsi-panel-j2-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 07 FF/39 01 00 00 00 00 03 51 07 FF/g' ${dts_source}/dsi-panel-j2-p1-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 0F FF/39 01 00 00 00 00 03 51 0F FF/g' ${dts_source}/dsi-panel-j1u-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 0F FF/39 01 00 00 00 00 03 51 0F FF/g' ${dts_source}/dsi-panel-j2-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 03 51 0F FF/39 01 00 00 00 00 03 51 0F FF/g' ${dts_source}/dsi-panel-j2-p1-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 05 51 07 FF 00 00/39 01 00 00 00 00 05 51 07 FF 00 00/g' ${dts_source}/dsi-panel-j1s-42-02-0a-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 05 51 07 FF 00 00/39 01 00 00 00 00 05 51 07 FF 00 00/g' ${dts_source}/dsi-panel-j1s-42-02-0a-mp-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 05 51 07 FF 00 00/39 01 00 00 00 00 05 51 07 FF 00 00/g' ${dts_source}/dsi-panel-j2-mp-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 05 51 07 FF 00 00/39 01 00 00 00 00 05 51 07 FF 00 00/g' ${dts_source}/dsi-panel-j2-p2-1-42-02-0b-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 00 00 05 51 07 FF 00 00/39 01 00 00 00 00 05 51 07 FF 00 00/g' ${dts_source}/dsi-panel-j2s-mp-42-02-0a-dsc-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 01 00 03 51 03 FF/39 01 00 00 01 00 03 51 03 FF/g' ${dts_source}/dsi-panel-j11-38-08-0a-fhd-cmd.dtsi
-sed -i 's/\/\/39 01 00 00 11 00 03 51 03 FF/39 01 00 00 11 00 03 51 03 FF/g' ${dts_source}/dsi-panel-j2-p2-1-38-0c-0a-dsc-cmd.dtsi
-
-    make $MAKE_ARGS ${TARGET_DEVICE}_defconfig
-
-    make $MAKE_ARGS -j$(nproc --all) 2> >(tee -a error.log >&2)
-
-    if [ -f "out/arch/arm64/boot/Image" ]; then
-        echo "The file [out/arch/arm64/boot/Image] exists. MIUI Build successfully."
-    else
-        echo "The file [out/arch/arm64/boot/Image] does not exist. Seems MIUI build failed."
-        exit 1
-    fi
-
-    echo "Generating [out/arch/arm64/boot/dtb]......"
-    find out/arch/arm64/boot/dts -name '*.dtb' -exec cat {} + >out/arch/arm64/boot/dtb
-
-    rm -rf anykernel/kernels/
-    mkdir -p anykernel/kernels/
-    cp out/arch/arm64/boot/Image anykernel/kernels/
-    cp out/arch/arm64/boot/dtb anykernel/kernels/
-
-    cd anykernel
-    ZIP_FILENAME=Kernel_BloodReaper_MIUI_${TARGET_DEVICE}_$(date +'%Y%m%d_%H%M%S')_anykernel3_${GIT_COMMIT_ID}.zip
-    zip -r9 $ZIP_FILENAME ./* -x .git .gitignore out/ ./*.zip
-    mv $ZIP_FILENAME ../
-    cd ..
-
-    echo "Build for MIUI/HOS finished."
-fi
 
 echo "Done. The flashable zip is: [./$ZIP_FILENAME]"
