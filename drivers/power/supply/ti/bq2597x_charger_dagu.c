95b9e09dc244c3b09562ee94d7ec5c08f9a81da1
CuriousNom <aryanbiswas7585@gmail.com>
2025-04-27 20:47:26 +0530

    drivers: power: Reset xiaomi drivers

--- drivers/power/supply/ti/bq2597x_charger_dagu.c
+++ drivers/power/supply/ti/bq2597x_charger_dagu.c
@@ -1103,8 +1103,10 @@ static int bq2597x_get_adc_data(struct bq2597x *bq, int channel, int *result)
 		*result = t;
 		/* vbat need calibration read by NU2105 */
 		if (channel == ADC_VBAT) {
-			t = t * (1 + 1803 / 1000000);
+			kernel_neon_begin();
+			t = t * (1 + 1.803 * 0.001);
 			*result = t;
+			kernel_neon_end();
 		}
 	} else {
 		ret = bq2597x_read_word(bq, ADC_REG_BASE + (channel << 1),
